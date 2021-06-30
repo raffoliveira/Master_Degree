5de02f391f603fbaf53a38b65dff9e52620443d1
Rafael_Oliveira <rafaelollywer@gmail.com>
2021-06-30 15:52:37 -0300

    Update methodology exercises

---
+++ Disciplinas/Metodologia/probabilidade/media.cpp
@@ -0,0 +1,132 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+
+template <typename T>
+void printVector(std::vector<T> &vec)
+{
+    std::cout << "[";
+    for (auto i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i];
+        if (i < vec.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]";
+    std::cout << std::endl;
+}
+
+std::vector<double> readingFile()
+{
+
+    std::vector<double> array;
+    std::string line;
+    std::string path = "C:\\Users\\rafae\\Documents\\GitHub\\Master_Degree\\Disciplinas\\Metodologia\\probabilidade\\";
+    std::fstream readFile(path + "medium.txt");
+
+    if (readFile.is_open())
+    {
+        while (getline(readFile, line)) //reading line by line
+        {
+            if (line.size() > 0)
+            {
+                int number = stoi(line); //casting from string to int
+                array.push_back(number);
+            }
+        }
+        readFile.close();
+    }
+    return array;
+}
+
+double mean(const std::vector<double> &data)
+{
+    double sum = std::accumulate(data.begin(), data.end(), 0);
+
+    return sum / data.size();
+}
+
+double mode(const std::vector<double> &data)
+{
+    double mostElement = data.front();
+    int maxCount = 0;
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        int c = std::count(data.begin(), data.end(), data[i]);
+        if (c > maxCount)
+        {
+            maxCount = c;
+            mostElement = data[i];
+        }
+    }
+
+    return mostElement;
+}
+
+int partition(std::vector<double> &vec, int l, int r)
+{
+    double pivot = vec[r];
+    int i = (l - 1);
+
+    for (int j = l; j <= r - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (vec[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            std::swap(vec[i], vec[j]);
+        }
+    }
+    std::swap(vec[i + 1], vec[r]);
+
+    return (i + 1);
+}
+
+void quickSort(std::vector<double> &vec, int l, int r)
+{
+    if (l < r)
+    {
+        int s = partition(vec, l, r);
+        quickSort(vec, l, s - 1);
+        quickSort(vec, s + 1, r);
+    }
+}
+
+double percentile(const std::vector<double> &data, int p)
+{
+    std::vector<double> v(data);
+    quickSort(v, 0, v.size() - 1);
+
+    int q = std::ceil((p * (data.size() - 1)) / 100);
+
+    return v[q];
+}
+
+double median(const std::vector<double> &data)
+{
+    return percentile(data, 50);
+}
+
+int main()
+{
+    std::vector<double> v = readingFile();
+    // std::vector<double> v({1, 5, 5, 5, 4});
+    int per = 75;
+
+    printVector(v);
+
+    std::cout << "The mean is " << mean(v) << std::endl;
+    std::cout << "The mode is " << mode(v) << std::endl;
+    std::cout << "The median is " << median(v) << std::endl;
+    std::cout << "The percentile " << per << " is " << percentile(v, per) << std::endl;
+
+    return 0;
+}
--- Disciplinas/PAA/listas/lista_6/exercicios/dynamic_programming/coin_collecting.cpp
+++ Disciplinas/PAA/listas/lista_6/exercicios/dynamic_programming/coin_collecting.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 #include <limits>
 
+int coinCollecting_rec(std::vector<std::vector<int>> &board, int i, int j)
+{
+    if (i == 0 && j == 0)
+    {
+        return board[i][j];
+    }
+    else if (i == 0)
+    {
+        return coinCollecting_rec(board, i, j - 1) + board[i][j];
+    }
+    else if (j == 0)
+    {
+        return coinCollecting_rec(board, i - 1, j) + board[i][j];
+    }
+    else
+    {
+        return std::max(coinCollecting_rec(board, i, j - 1), coinCollecting_rec(board, i - 1, j)) + board[i][j];
+    }
+}
+
 int coinCollecting_dynamic(std::vector<std::vector<int>> &board, int line, int column)
 {
     std::vector<std::vector<int>> aux(line, std::vector<int>(column, 0));
@@ -15,12 +35,12 @@ int coinCollecting_dynamic(std::vector<std::vector<int>> &board, int line, int c
     for (int i = 1; i < line; ++i)
     {
         aux[i][0] = aux[i - 1][0] + board[i][0];
+
         for (int j = 1; j < column; ++j)
         {
             aux[i][j] = std::max(aux[i - 1][j], aux[i][j - 1]) + board[i][j];
         }
     }
-
     return aux[line - 1][column - 1];
 }
 
@@ -33,7 +53,8 @@ int main()
                                            {0, 0, 1, 0, 0, 1},
                                            {1, 0, 0, 0, 1, 0}};
 
-    std::cout << "The minimun number of coins is " << coinCollecting_dynamic(board, 5, 6) << std::endl;
+    // std::cout << "The minimun number of coins is " << coinCollecting_dynamic(board, 5, 6) << std::endl;
+    std::cout << "The minimun number of coins is " << coinCollecting_rec(board, 5 - 1, 6 - 1) << std::endl;
 
     return 0;
 }
