be1640263022189dcfc88a1c39c16fc93c264024
Rafael_Oliveira <rafaelollywer@gmail.com>
2021-07-07 18:54:00 -0300

    update methodology exercises

--- Disciplinas/Metodologia/probabilidade/intervalo_confianca1.cpp
+++ Disciplinas/Metodologia/probabilidade/intervalo_confianca1.cpp
@@ -6,30 +6,27 @@
 #include <numeric>
 #include <algorithm>
 #include <utility>
+#include <random>
 #include <cmath>
 #include <math.h>
 
-std::vector<double> readingFile()
+std::default_random_engine g;
+std::normal_distribution<float> n(1.0, 50.0);
+
+template <typename T>
+void printVector(std::vector<T> &vec)
 {
-
-    std::vector<double> array;
-    std::string line;
-    std::string path = "C:\\Users\\rafae\\Documents\\GitHub\\Master_Degree\\Disciplinas\\Metodologia\\probabilidade\\tests\\";
-    std::fstream readFile(path + "medium.txt");
-
-    if (readFile.is_open())
+    std::cout << "[";
+    for (auto i = 0; i < vec.size(); i++)
     {
-        while (getline(readFile, line)) //reading line by line
+        std::cout << vec[i];
+        if (i < vec.size() - 1)
         {
-            if (line.size() > 0)
-            {
-                int number = stoi(line); //casting from string to int
-                array.push_back(number);
-            }
+            std::cout << ", ";
         }
-        readFile.close();
     }
-    return array;
+    std::cout << "]";
+    std::cout << std::endl;
 }
 
 double mean(const std::vector<double> &data)
@@ -60,6 +57,7 @@ double normal_distribution_cdf1(double x, double mu, double std_dev)
     return ((1 + (std::erf((x - mu) / (std_dev * std::sqrt(2))))) / 2);
 }
 
+//mean = 0 and std_dev = 1
 double normal_distribution_cdf2(double x)
 {
     return ((1 + (std::erf(x / std::sqrt(2)))) / 2);
@@ -68,32 +66,34 @@ double normal_distribution_cdf2(double x)
 //assumindo que x2 > x1
 double normal_distribution_cdf3(double x1, double x2, double mu, double std_dev)
 {
-    double result1 = normal_distribution_cdf1(x1, mu, std_dev);
-    double result2 = normal_distribution_cdf1(x2, mu, std_dev);
-
-    return result2 - result1;
+    return normal_distribution_cdf1(x2, mu, std_dev) - normal_distribution_cdf1(x1, mu, std_dev);
 }
 
 //assumindo que x2 > x1
 double normal_distribution_cdf4(double x1, double x2)
 {
-    double result1 = normal_distribution_cdf2(x1);
-    double result2 = normal_distribution_cdf2(x2);
-
-    return result2 - result1;
+    return normal_distribution_cdf2(x2) - normal_distribution_cdf2(x1);
 }
 
 int main()
 {
-    std::vector<double> v = readingFile();
-    double x = 50, x1 = 49, x2 = 52;
+    std::vector<double> v;
+
+    for (int i = 0; i < 500; i++)
+    {
+        v.push_back(n(g));
+    }
+
+    double x = 5, x1 = 3, x2 = 6;
     double mu = mean(v);
     double std_deviation = std_dev(v);
 
-    std::cout << "The probability 1 is " << normal_distribution_cdf1(x, mu, std_deviation) << std::endl;
-    std::cout << "The probability 2 is " << normal_distribution_cdf2(x) << std::endl;
-    std::cout << "The probability 1 between " << x1 << " e " << x2 << " is " << normal_distribution_cdf3(x1, x2, mu, std_deviation) << std::endl;
-    std::cout << "The probability 2 between " << x1 << " e " << x2 << " is " << normal_distribution_cdf4(x1, x2) << std::endl;
+    printVector(v);
+
+    std::cout << "The probability 1 of " << x << " is " << normal_distribution_cdf1(x, mu, std_deviation) << std::endl;
+    std::cout << "The probability 2 of " << x << " is " << normal_distribution_cdf2(x) << std::endl;
+    std::cout << "The probability 1 of " << x << " is between " << x1 << " e " << x2 << " is " << normal_distribution_cdf3(x1, x2, mu, std_deviation) << std::endl;
+    std::cout << "The probability 2 of " << x << " is between " << x1 << " e " << x2 << " is " << normal_distribution_cdf4(x1, x2) << std::endl;
 
     return 0;
 }
--- Disciplinas/Metodologia/probabilidade/normal_distribution.cpp
+++ Disciplinas/Metodologia/probabilidade/normal_distribution.cpp
@@ -7,6 +7,49 @@
 #include <algorithm>
 #include <utility>
 #include <cmath>
+#include <random>
+
+std::default_random_engine g;
+std::normal_distribution<float> n(1.0, 50.0);
+
+template <typename T>
+void printVector(std::vector<T> &vec)
+{
+    std::cout << "[";
+    for (auto i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i];
+        if (i < vec.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]";
+    std::cout << std::endl;
+}
+
+double mean(const std::vector<double> &data)
+{
+    double sum = std::accumulate(data.begin(), data.end(), 0);
+
+    return sum / data.size();
+}
+
+double variance(const std::vector<double> &data)
+{
+    double _mean = mean(data), sum = 0;
+
+    for (int i = 0; i < data.size(); ++i)
+    {
+        sum += std::pow((data[i] - _mean), 2);
+    }
+    return sum / (data.size() - 1);
+}
+
+double std_dev(const std::vector<double> &data)
+{
+    return std::sqrt(variance(data));
+}
 
 double normal_distribution_pdf1(double x, double mu, double std_dev)
 {
@@ -23,9 +66,18 @@ double normal_distribution_pdf2(double x)
 
 int main()
 {
-    double x = 1.4, mu = 1.56, std_dev = 1.87;
+    std::vector<double> v;
 
-    std::cout << "The probability 1 is " << normal_distribution_pdf1(x, mu, std_dev) << std::endl;
+    for (int i = 0; i < 500; i++)
+    {
+        v.push_back(n(g));
+    }
+
+    double x = 5;
+    double mu = mean(v);
+    double std_deviation = std_dev(v);
+
+    std::cout << "The probability 1 is " << normal_distribution_pdf1(x, mu, std_deviation) << std::endl;
     std::cout << "The probability 2 is " << normal_distribution_pdf2(x) << std::endl;
 
     return 0;
